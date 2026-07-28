a294b60e12ae2be58b3feda9eba80609dfc1119f
moreveal <morrisreveal@gmail.com>
2026-07-28 19:42:15 +0300

    fix(modules): publish FunPay 1.3.9 update

--- src/buywell_edge/official_packages.py
+++ src/buywell_edge/official_packages.py
@@ -20,10 +20,10 @@ OFFICIAL_PACKAGES = {
     item.reference: item
     for item in (
         OfficialPackage(
-            reference="funpay.cardinal@1.3.7",
-            filename="funpay.cardinal-1.3.7.buywell-edge.zip",
-            download_url="https://github.com/moreveal/buywell-runtimes/releases/download/funpay.cardinal-v1.3.7/funpay.cardinal-1.3.7.buywell-edge.zip",
-            archive_sha256="c40635a4015fa7c40d8bdff4f59a1f9bd940b28bfac0d4310d5200b0fd5c261b",
+            reference="funpay.cardinal@1.3.9",
+            filename="funpay.cardinal-1.3.9.buywell-edge.zip",
+            download_url="https://github.com/moreveal/buywell-runtimes/releases/download/funpay.cardinal-v1.3.9/funpay.cardinal-1.3.9.buywell-edge.zip",
+            archive_sha256="8b47954f94180d6012c391289309215546696c2b41a910c70dabaf28bdf0fab7",
             public_key=_PUBLIC_KEY,
         ),
         OfficialPackage(
